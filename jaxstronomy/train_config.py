75c1f3ccb2504b36d0ac94d38427153269b206e9
Sebastian Wagner-Carena <sebastianwagner-carena@Sebastians-MacBook-Pro.local>
2023-02-25 02:50:43 -0800

    smaller batch size because of the memory required by all the substructure

--- jaxstronomy/train_config.py
+++ jaxstronomy/train_config.py
@@ -9,14 +9,14 @@ def get_config():
     config.model = 'ResNet50'
 
     config.momentum = 0.9
-    config.batch_size = 128
+    config.batch_size = 64
 
     config.cache = False
     config.half_precision = False
 
     # If num_train_steps==-1 then the number of training steps is calculated from
     # num_epochs using the entire dataset. Similarly for steps_per_eval.
-    config.steps_per_epoch = 1950
+    config.steps_per_epoch = 7800
     config.warmup_steps = 10 * config.steps_per_epoch
     config.num_train_steps = 500 * config.steps_per_epoch
     config.keep_every_n_steps = 500
