68bfccc1025768357bb4262b7433c5e25a6b571e
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2023-09-27 14:28:57 -0400

    new configs assume 4 gpus

--- jaxstronomy/TrainConfigs/train_config_snpe_e_qd.py
+++ jaxstronomy/TrainConfigs/train_config_snpe_e_qd.py
@@ -17,7 +17,7 @@ def get_config():
 
     # Need to set the boundaries of how long the model will train generically
     # and when the sequential training will turn on.
-    config.steps_per_epoch = 15600
+    config.steps_per_epoch = 3900 # Assuming 4 GPUs
     config.num_initial_train_steps = config.steps_per_epoch * 100
     config.num_steps_per_refinement = config.steps_per_epoch * 10
     config.num_train_steps = config.steps_per_epoch * 500
