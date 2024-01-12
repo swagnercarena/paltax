69b8f00b025adc584d046872032e19d13b4d7f6e
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2024-01-12 12:40:44 -0500

    updated basic training configuration

--- jaxstronomy/TrainConfigs/train_config.py
+++ jaxstronomy/TrainConfigs/train_config.py
@@ -9,16 +9,16 @@ def get_config():
     config.model = 'ResNet50'
 
     config.momentum = 0.9
-    config.batch_size = 64
+    config.batch_size = 32
 
     config.cache = False
     config.half_precision = False
 
     # If num_train_steps==-1 then the number of training steps is calculated from
     # num_epochs using the entire dataset. Similarly for steps_per_eval.
-    config.steps_per_epoch = 15600
+    config.steps_per_epoch = 3900
     config.num_train_steps = 500 * config.steps_per_epoch
-    config.keep_every_n_steps = 500
+    config.keep_every_n_steps = config.steps_per_epoch
 
     # Parameters of the learning rate schedule
     config.schedule_function_type = 'cosine'
