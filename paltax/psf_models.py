a3b14646b0195b14fded1c080b1f1c998ee70a28
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2024-03-13 16:44:18 -0400

    fixed formatting and license statement at top of files

--- paltax/psf_models.py
+++ paltax/psf_models.py
@@ -1,26 +1,11 @@
 # coding=utf-8
-# Copyright 2022 The Google Research Authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# Copyright 2022 Google LLC
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -51,68 +36,69 @@ class _PSFModelBase():
     parameters = ()
 
     def modify_cosmology_params(
-            self: Any,
-            cosmology_params: Mapping[str, Union[float, int, jnp.ndarray]]
-        ) -> Mapping[str, Union[float, int, jnp.ndarray]]:
-        """Modify cosmology params to include information required by model.
+        self: Any,
+        cosmology_params: Mapping[str, Union[float, int, jnp.ndarray]]
+    ) -> Mapping[str, Union[float, int, jnp.ndarray]]:
+            """Modify cosmology params to include information required by model.
 
-        Args:
-            cosmology_params: Cosmological parameters that define the universe's
-                expansion.
+            Args:
+                    cosmology_params: Cosmological parameters that define the universe's
+                            expansion.
 
-        Returns:
-            Modified cosmology parameters.
-        """
-        return cosmology_params
+            Returns:
+                    Modified cosmology parameters.
+            """
+            return cosmology_params
 
 
 class Gaussian(_PSFModelBase):
-  """Implementation of Gaussian point spread function."""
+    """Implementation of Gaussian point spread function."""
 
-  parameters = ('fwhm', 'pixel_width')
+    parameters = ('fwhm', 'pixel_width')
 
-  @staticmethod
-  def convolve(
-      image,
-      kwargs_psf):
-    """Convolve an image with the Gaussian point spread function.
+    @staticmethod
+    def convolve(
+            image,
+            kwargs_psf):
+        """Convolve an image with the Gaussian point spread function.
 
-    Args:
-      image: Image to convolve
-      kwargs_psf: Keyword arguments defining the point spread function.
+        Args:
+            image: Image to convolve
+            kwargs_psf: Keyword arguments defining the point spread function.
 
-    Returns:
-      Convolved image.
-    """
-    sigma_angular = kwargs_psf['fwhm'] / (2 * jnp.sqrt(2 * jnp.log(2)))
-    sigma_pixel = sigma_angular / kwargs_psf['pixel_width']
+        Returns:
+            Convolved image.
+        """
+        sigma_angular = kwargs_psf['fwhm'] / (2 * jnp.sqrt(2 * jnp.log(2)))
+        sigma_pixel = sigma_angular / kwargs_psf['pixel_width']
 
-    # dm_pix expects channel dimensions.
-    return dm_pix.gaussian_blur(
-        jnp.expand_dims(image, axis=-1), sigma_pixel, kernel_size=30)[:, :, 0]
+        # dm_pix expects channel dimensions.
+        return dm_pix.gaussian_blur(
+                jnp.expand_dims(image, axis=-1), sigma_pixel, kernel_size=30
+            )[:, :, 0]
 
 
 class Pixel(_PSFModelBase):
-  """Implementation of Pixel point spread function."""
+    """Implementation of Pixel point spread function."""
 
-  parameters = ('kernel_point_source',)
+    parameters = ('kernel_point_source',)
 
-  @staticmethod
-  def convolve(
-      image,
-      kwargs_psf):
-    """Convolve an image with the pixel point spread function.
+    @staticmethod
+    def convolve(
+            image,
+            kwargs_psf):
+        """Convolve an image with the pixel point spread function.
 
-    Args:
-      image: Image to convolve
-      kwargs_psf: Keyword arguments defining the point spread function.
+        Args:
+            image: Image to convolve
+            kwargs_psf: Keyword arguments defining the point spread function.
 
-    Returns:
-      Convolved image.
-    """
-    # Always normalize kernel to 1 to avoid user error.
-    kernel = (
-        kwargs_psf['kernel_point_source'] /
-        jnp.sum(kwargs_psf['kernel_point_source']))
-    return jax.scipy.signal.convolve(
-        image, kernel, mode='same')
+        Returns:
+            Convolved image.
+        """
+        # Always normalize kernel to 1 to avoid user error.
+        kernel = (
+                kwargs_psf['kernel_point_source'] /
+                jnp.sum(kwargs_psf['kernel_point_source']))
+        return jax.scipy.signal.convolve(
+                image, kernel, mode='same')
--- paltax/source_models_test.py
+++ paltax/source_models_test.py
@@ -1,10 +1,10 @@
 # coding=utf-8
-#
+
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#     https://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -443,7 +443,9 @@ class CosmosCatalogTest(chex.TestCase, parameterized.TestCase):
             source_models.CosmosCatalog.k_correct_image
         )
 
-        self.assertAlmostEqual(expected, k_correct_image(z_old, z_new), places=6)
+        self.assertAlmostEqual(
+            expected, k_correct_image(z_old, z_new), places=6
+        )
 
 if __name__ == '__main__':
     absltest.main()
