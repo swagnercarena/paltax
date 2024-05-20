4e02d67c648c00de1a5522b107ec40748884953a
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2024-05-20 07:53:12 -0400

    train configurations for limited sources.

---
+++ paltax/InputConfigs/input_config_sr_50p.py
@@ -0,0 +1,29 @@
+# coding=utf-8
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Configuration file using only 50 percent of sources.
+"""
+
+from  paltax.InputConfigs import input_config_br
+from paltax.input_pipeline import encode_uniform
+
+def get_config():
+    """Get the hyperparameter configuration"""
+    config = input_config_br.get_config()
+
+    # Limit the number of unique batches.
+    config['source_params']['galaxy_index'] = encode_uniform(
+        minimum=0.0, maximum=0.5
+    )
+
+    return config
---
+++ paltax/TrainConfigs/train_config_snpe_sr_10p.py
@@ -0,0 +1,15 @@
+import pathlib
+
+from paltax.TrainConfigs import train_config_snpe_base
+
+
+def get_config():
+    """Get the default hyperparameter configuration."""
+    config = train_config_snpe_base.get_config()
+
+    # Search for the input configuration relative to this config file to ease
+    # use accross filesystems.
+    config.input_config_path = str(pathlib.Path(__file__).parent)
+    config.input_config_path += '/../InputConfigs/input_config_sr_10p.py'
+
+    return config
