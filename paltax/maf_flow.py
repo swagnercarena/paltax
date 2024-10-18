batch normalization fixes.

paltax/maf_flow.py
@@ -719,7 +719,8 @@ class EmbeddedFlow(nn.Module):
     flow_module: ModuleDef
 
     def __call__(
-        self, y: jnp.ndarray, unembedded_context: jnp.ndarray
+        self, y: jnp.ndarray, unembedded_context: jnp.ndarray,
+        train: Optional[bool] = True
     ) -> jnp.ndarray:
         """Return log probability of transformed random variables.
 
@@ -727,15 +728,18 @@ class EmbeddedFlow(nn.Module):
             y: Transformed random variables.
             unembedded_context: Context for flow transformation before
                 embedding. This should be the raw data.
+            train: Whether or not the model is being trained and batch
+                statistics should be updated.
 
         Returns:
             Log probability of transformed random variables.
         """
-        context = self.embed_context(unembedded_context)
+        context = self.embed_context(unembedded_context, train)
         return self.flow_module(y, context)
 
     def call_apt(
-        self, y_apt: jnp.ndarray, unembedded_context: jnp.ndarray
+        self, y_apt: jnp.ndarray, unembedded_context: jnp.ndarray,
+        train: Optional[bool] = True
     ) -> jnp.ndarray:
         """Return log probability of transformed random variables.
 
@@ -745,30 +749,36 @@ class EmbeddedFlow(nn.Module):
                 unembedded_context).
             unembedded_context: Context for flow transformation before
                 embedding. This should be the raw data.
+            train: Whether or not the model is being trained and batch
+                statistics should be updated.
 
         Returns:
             Log probability of transformed random variables.
         """
-        context = self.embed_context(unembedded_context)
+        context = self.embed_context(unembedded_context, train)
         return jax.vmap(self.flow_module, in_axes=[1, None], out_axes=1)(
             y_apt, context
         )
 
-    def embed_context(self, unembedded_context: jnp.ndarray) -> jnp.ndarray:
+    def embed_context(
+        self, unembedded_context: jnp.ndarray, train: bool
+    ) -> jnp.ndarray:
         """Embed context using embedding network.
 
         Args:
             unembedded_context: Context for flow transformation before
                 embedding. This should be the raw data.
+            train: Whether or not the model is being trained and batch
+                statistics should be updated.
 
         Returns:
             Context after embedding.
         """
-        return self.embedding_module(unembedded_context)
+        return self.embedding_module(unembedded_context, train)
 
     def sample(
         self, rng: List[int], unembedded_context: jnp.ndarray,
-        sample_shape: List[int]
+        sample_shape: List[int], train: Optional[bool] = True
     ) -> jnp.ndarray:
         """Draw samples from transformed distribution.
 
@@ -777,6 +787,8 @@ class EmbeddedFlow(nn.Module):
             unembedded_context: Context for flow transformation before
                 embedding. This should be the raw data.
             sample_shape: Desired output shape of samples.
+            train: Whether or not the model is being trained and batch
+                statistics should be updated.
 
         Returns:
             Samples of transformed variables.
@@ -786,7 +798,7 @@ class EmbeddedFlow(nn.Module):
             in it (i.e. batch norm). Passing in only one point of context may
             may lead to unexpected behavior.
         """
-        context = self.embed_context(unembedded_context)
+        context = self.embed_context(unembedded_context, train)
         rng_sample = jax.random.split(rng, len(context))
         return jax.vmap(self.flow_module.sample, in_axes=[0, 0, None])(
             rng_sample, context, sample_shape
@@ -794,7 +806,7 @@ class EmbeddedFlow(nn.Module):
 
     def sample_and_log_prob(
         self, rng: List[int], unembedded_context: jnp.ndarray,
-        sample_shape: List[int]
+        sample_shape: List[int], train: Optional[bool] = True
     ) -> jnp.ndarray:
         """Draw samples and corresponding log probability of the sample.
 
@@ -803,6 +815,8 @@ class EmbeddedFlow(nn.Module):
             unembedded_context: Context for flow transformation before
                 embedding. This should be the raw data.
             sample_shape: Desired output shape of samples.
+            train: Whether or not the model is being trained and batch
+                statistics should be updated.
 
         Returns:
             Samples of transformed variables and log probability.
@@ -812,7 +826,7 @@ class EmbeddedFlow(nn.Module):
             in it (i.e. batch norm). Passing in only one point of context may
             may lead to unexpected behavior.
         """
-        context = self.embed_context(unembedded_context)
+        context = self.embed_context(unembedded_context, train)
         rng_sample = jax.random.split(rng, len(context))
         return jax.vmap(
             self.flow_module.sample_and_log_prob, in_axes=[0, 0, None]

paltax/maf_flow_test.py
@@ -569,7 +569,8 @@ class EmbeddedFlowTest(chex.TestCase):
             rng, jnp.ones((1, n_dim)), jnp.ones((1, image_size, image_size, 1))
         )
         apply_fn = self.variant(
-            maf.apply, static_argnames=['method', 'sample_shape', 'mutable']
+            maf.apply,
+            static_argnames=['method', 'sample_shape', 'mutable', 'train']
         )
 
         batch_size = 4
@@ -596,7 +597,8 @@ class EmbeddedFlowTest(chex.TestCase):
 
         # Test embed_context function.
         embed_context, _ = apply_fn(
-            params, context, method='embed_context', mutable=('batch_stats',)
+            params, context, method='embed_context', mutable=('batch_stats',),
+            train=True
         )
 
         # Test sampling functions.

paltax/train_nf.py
@@ -13,11 +13,10 @@
 # limitations under the License.
 """Training script for normalizing flow dark matter substructure inference."""
 
-import bisect
 import copy
 import functools
 import time
-from typing import Any, Callable, Dict, Iterator, Mapping, Optional, Sequence, Tuple, Union
+from typing import Any, Callable, Dict, Mapping, Optional, Sequence, Tuple, Union
 
 from clu import metric_writers
 from clu import periodic_actions
@@ -25,7 +24,6 @@ from flax import jax_utils
 from flax.training import checkpoints
 from flax.training import common_utils
 import jax
-from jax import lax
 import jax.numpy as jnp
 import ml_collections
 import optax
@@ -235,20 +233,20 @@ def draw_sample(
 
 
 def extract_flow_context(
-    state: TrainState, target_image: jnp.ndarray, image_batch: jnp.ndarray
+    state: TrainState, target_image: jnp.ndarray
 ) -> Tuple[Mapping[str, Mapping[str, jnp.ndarray]], jnp.ndarray]:
     """Extract flow parameters and the target image context.
 
     Args:
         state: Current TrainState object for the model.
         target_image: Target image for context.
-        image_batch: Batch of images for batch normalization purposes.
 
     Returns:
         Flow parameters and encoded context.
     """
     # Insert image into the training batch.
-    image_batch = image_batch.at[0].set(target_image)
+    image_batch = jnp.expand_dims(target_image, axis=0)
+
     # Extract the flow parameters.
     flow_params = {
         'params': state.params['flow_module']
@@ -257,11 +255,10 @@ def extract_flow_context(
     context, _ = state.apply_fn(
         {'params': state.params, 'batch_stats': state.batch_stats},
         image_batch, mutable=['batch_stats'],
-        method='embed_context'
+        method='embed_context', train=False
     )
-    # We only want the context for the target image.
-    context = context[0]
-    return flow_params, context
+
+    return flow_params, context[0] # Remove batch dimension.
 
 
 def get_flow_weight_schedule(
@@ -560,12 +557,6 @@ def train_and_evaluate_nf(
 
     # Jit compile the function for extracting the flow parrameters and context.
     extract_flow_context_pmap = jax.pmap(extract_flow_context)
-    # Create an artificial batch of initial images in case we are restarting the
-    # model.
-    image = jax_utils.replicate(jnp.tile(
-        jnp.expand_dims(target_image, axis=-1),
-        (config.batch_size, 1, 1, 1)
-    ))
     target_image = jax_utils.replicate(jnp.expand_dims(target_image, axis=-1))
 
     train_metrics = []
@@ -579,7 +570,7 @@ def train_and_evaluate_nf(
     # This restart isn't perfect, but we're not going to load weights again
     # so we'll just use whatever model we have now.
     flow_params, context = extract_flow_context_pmap(
-        state, target_image, image
+        state, target_image
     )
 
     for step in range(step_offset, num_steps):
@@ -588,7 +579,7 @@ def train_and_evaluate_nf(
         if step in refinement_step_list:
             print(f'Saving new flow weights for sampling at step {step}')
             flow_params, context = extract_flow_context_pmap(
-                state, target_image, image
+                state, target_image
             )
 
         # Generate truths and images